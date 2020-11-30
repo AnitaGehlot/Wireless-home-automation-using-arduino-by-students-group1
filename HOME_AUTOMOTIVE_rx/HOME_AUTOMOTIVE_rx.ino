d7166ff42b5cc068980804198c2d53496bb4153d
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 11:09:27 +0530

    TX/RX code and model

---
+++ HOME_AUTOMOTIVE_rx/HOME_AUTOMOTIVE_rx.ino
@@ -0,0 +1,51 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(9,10,5,6,7,8);
+
+int FAN_PIN = 2;
+int BULB_PIN = 4;
+
+void setup()
+{
+  lcd.begin(20,2);
+  Serial.begin(9600);
+  
+  pinMode(FAN_PIN,OUTPUT);
+  pinMode(BULB_PIN,OUTPUT);
+}
+
+void loop()
+{
+  lcd.clear();
+  int signal=Serial.read();
+  if(signal==30){
+    digitalWrite(FAN_PIN,HIGH);
+    digitalWrite(BULB_PIN,HIGH);
+    lcd.print("Fan Switched ON");
+    lcd.setCursor(0,1);
+    lcd.print("Bulbs Switched ON");
+    delay(100);
+  }
+  else if(signal==10){
+    digitalWrite(BULB_PIN,HIGH);
+    lcd.print("Fan Switched OFF");
+    lcd.setCursor(0,1);
+    lcd.print("Bulbs Switched ON");
+    delay(100);
+  }
+  else if(signal==20){
+    digitalWrite(FAN_PIN,HIGH);
+    lcd.print("Fan Switched ON");
+    lcd.setCursor(0,1);
+    lcd.print("Bulbs Switched OFF");
+    delay(100);
+  }
+  else {
+    digitalWrite(FAN_PIN,LOW);
+    digitalWrite(BULB_PIN,LOW);
+    lcd.print("Fan Switched OFF");
+    lcd.setCursor(0,1);
+    lcd.print("Bulbs Switched OFF");
+  }
+  delay(100);
+}
---
+++ HOME_AUTOMOTIVE_tx/HOME_AUTOMOTIVE_tx.ino
@@ -0,0 +1,35 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(10,9,8,7,6,5);
+
+int SWITCH1 = 4;
+int SWITCH2 = 2;
+
+void setup()
+{
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  
+  pinMode(SWITCH1,INPUT_PULLUP);
+  pinMode(SWITCH2,INPUT_PULLUP);
+  
+  lcd.print("BUTTON 1 -> Bulbs");
+  lcd.setCursor(0,1);
+  lcd.print("BUTTON 2 -> Fan");
+}
+
+void loop()
+{
+  int bulb=10;
+  int fan=20;
+  int both=30;
+  
+  if(digitalRead(SWITCH2)==LOW && digitalRead(SWITCH1)==LOW)
+    Serial.write(both);
+  else if(digitalRead(SWITCH1)==LOW)
+    Serial.write(bulb);
+  else if(digitalRead(SWITCH2)==LOW)
+    Serial.write(fan);
+    
+  delay(100);
+}
